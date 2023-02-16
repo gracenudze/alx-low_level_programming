eb465063557f65da2bd14891935c6c18ef220b44
gracenudze <graceynud@gmail.com>
2023-02-16 11:20:49 -0800

    program that prints exactly Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - the main is a programe that prits a puts function
+* Return: 0 (this is to say your programe was successful)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
